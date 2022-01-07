cffb4046b27a0505974a46dc2956a81115999737
AhmadMous <badmass95@gmail.com>
2022-01-07 20:54:05 +0200

    solve 6.4

--- Ch6/6.3 Iterable Groups.js
+++ Ch6/6.3 Iterable Groups.js
@@ -63,10 +63,3 @@ class GroupIterator{                                   // group iterator class
         return value;                                  
     }
 }
-
-// for (let value of Group.from(["a", "b", "c"])) {
-//     console.log(value);
-//   }
-//   // → a
-//   // → b
-//   // → c
---
+++ Ch6/6.4 Borrowing a Method.js
@@ -0,0 +1,10 @@
+// Earlier in the chapter I mentioned that an object’s hasOwnProperty can be
+// used as a more robust alternative to the in operator when you want to ignore
+// the prototype’s properties. But what if your map needs to include the word
+// "hasOwnProperty" ? You won’t be able to call that method anymore because the
+// object’s own property hides the method value.
+// Can you think of a way to call hasOwnProperty on an object that has its own
+// property by that name?
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));   // just call the same function from object prototype, use call method with first arg being map as a replacement of implicit "this"
+// alternatively
